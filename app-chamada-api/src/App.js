48540356427b411558316f97b280ae1db71d123e
Evely-27 <evelysantos27@gmail.com>
2022-05-17 22:22:23 -0300

    criando um array simples para preparar a tabela de forma dinamica

--- app-chamada-api/src/App.js
+++ app-chamada-api/src/App.js
@@ -1,4 +1,16 @@
 function App() {
+
+// vamos criar um array para deixar a tabela dinamica, para prepara-la para a api
+// vamos criar um array javascript
+// os atributos pegamos como mostrado na api
+
+  const lista_users = [
+    
+      {name: "User1", email: "user1.teste@gmail.com", created_at: "2022-02-02"},
+      {name: "User2", email: "user2.teste@gmail.com", created_at: "2022-04-12"},
+      {name: "User34", email: "user34.teste@gmail.com", created_at: "2022-05-10"},
+  
+  ]
   return (
       <div>
         <table border="1">
@@ -10,13 +22,16 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Evely</td>
-              <td>Evely.santos@gmail</td>
-              <th>17/05/2022</th>
-            </tr>
+{/* Para utilizarmos javascript dentro do html do react, ele deve estar entre chaves {}
+vamos utilizzar a função map do javascript =  invoca a função callback (passada por argumento) para cada elemento do Array e devolve um novo Array como resultado. */}
+            { lista_users.map(user => {
+            return  <tr>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <th>{user.created_at}</th>
+                    </tr>
+              })}
           </tbody>
-
         </table>
       </div>
   );
